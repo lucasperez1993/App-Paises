aa2d908b2c34962eac7d203813c6951441110387
Lucas <lucasperez@gmail.com>
2022-11-07 10:13:24 -0300

    final

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,12 +5,21 @@ import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
+  styles: [
+    `
+      li{
+        cursor: pointer;
+      }
+    `
+  ]
 })
 export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
+  mostrarSugerencias: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
 
   constructor( private paisService: PaisService ){}
 
@@ -32,5 +41,17 @@ export class PorPaisComponent {
   
   sugerencias(termino: string) {
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+    
+    this.paisService.buscarPais(termino)
+        .subscribe(paises => this.paisesSugeridos = paises.splice(0,5),
+        (err) => 
+          this.paisesSugeridos = []
+        );
+  }
+
+  buscarSugerido(termino: string){
+    this.buscar(termino);
   }
 }
